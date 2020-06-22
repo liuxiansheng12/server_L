774cd90994755c0ae892cb31de1c19fb69f50e98
liuxiansheng12 <2499302263@qq.com>
2020-06-23 07:00:37 +0800

    2020/6/23-4

--- Service/nongChang/setSeed.js
+++ Service/nongChang/setSeed.js
@@ -5,6 +5,7 @@ const mysqlSetSeed = require("../../Dao/nongChang/setSeed");
 function setSeed(request, response, callBack) {
 
     request.on("data", (data) => {
+        console.log(data);
         const pathData = JSON.parse(data);
         const seed = JSON.stringify(pathData.seed);
         console.log(pathData, seed);
