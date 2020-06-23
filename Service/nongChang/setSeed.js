8e6c4b09710f88410d9cf4fd9963ba4ff98d5b4a
liuxiansheng12 <2499302263@qq.com>
2020-06-23 08:13:14 +0800

    2020/6/23-9

--- Service/nongChang/setSeed.js
+++ Service/nongChang/setSeed.js
@@ -6,6 +6,8 @@ function setSeed(request, response, callBack) {
 
     request.on("data", (d) => {
         const data = d.toString();
+        console.log(pathData.seed);
+        console.log(typeof pathData.seed);
         const pathData = JSON.parse(data);
         const seed = JSON.stringify(pathData.seed);
 
